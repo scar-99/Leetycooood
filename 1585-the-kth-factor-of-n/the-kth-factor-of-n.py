9011c98e7106b9a383fc9f11320562c9986961d8
MD Rehan <rehanshamsi0987@gmail.com>
2026-09-21 16:19:56 +0530

    Time: 4 ms (25.88%) | Memory: 12.3 MB (89.22%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kthFactor(self, n, k):
+
+        for i in range(1, n + 1):
+
+            if n % i == 0:
+                k -= 1
+
+                if k == 0:
+                    return i
+
+        return -1
